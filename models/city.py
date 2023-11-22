f6ad8fe5043f820c97667ca59ee20f66fcfa4e7f
mefdev <mohamedmeftah2018@gmail.com>
2023-11-22 02:11:36 +0100

    Add a city table

--- models/city.py
+++ models/city.py
@@ -1,9 +1,23 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
+#!/usr/bin/python
+""" holds class City"""
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models import env_storage
+
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    """Representation of city """
+    if  env_storage == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
