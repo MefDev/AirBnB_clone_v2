bd07ce8a60934d20d7db19dded6e0d28a97cb06d
mefdev <mohamedmeftah2018@gmail.com>
2023-11-22 02:11:10 +0100

    Add Db storage functionality

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-from models import storage
-import uuid
+from models import storage, env_storage
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
+import uuid
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+
+
 Base = declarative_base()
+
 
 class BaseModel:
     """A base class for all hbnb models"""
+    if env_storage == "db":
+            id = Column(String(60), primary_key=True, nullable=False)
+            created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+            updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
-            self.id = Column(String(60), primary_key=True, nullable=False)
-            self.created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-            self.updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-            self.name = Column(String)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
