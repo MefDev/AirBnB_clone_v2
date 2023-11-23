cc3e2460f1db347f12b11cd75c4b8c3a8965eadd
mefdev <mohamedmeftah2018@gmail.com>
2023-11-23 04:05:44 +0100

    add review table

--- models/review.py
+++ models/review.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
-
+from models import env_storage
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+    if env_storage == "db":
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
