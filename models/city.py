c2d88a6babb9cbd17cfe53aca457e95e3617db0e
mefdev <mohamedmeftah2018@gmail.com>
2023-11-23 03:41:55 +0100

    Add a place relationship

--- models/city.py
+++ models/city.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ holds class City"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
 from models import env_storage
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -11,6 +12,8 @@ class City(BaseModel, Base):
     if env_storage == "db":
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete-orphan')
     else:
         state_id = ""
         name = ""
