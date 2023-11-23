059122fdbe956370fcdfb3eeb8dfc32359aff4d0
mefdev <mohamedmeftah2018@gmail.com>
2023-11-23 02:59:38 +0100

    setup the db_storage file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import environ
 from models.base_model import Base
-
-
+from models.state import State
+from models.city import City
 
 
 class DBStorage:
@@ -15,26 +16,22 @@ class DBStorage:
         """init dataabases"""
         dialect = 'mysql'
         driver = 'mysqldb'
-        
         # get environment variables
         user = environ.get('HBNB_MYSQL_USER')
         password = environ.get('HBNB_MYSQL_PWD')
         host = environ.get('HBNB_MYSQL_HOST')
         db = environ.get('HBNB_MYSQL_DB')
         env = environ.get('HBNB_ENV')
-
         # create the engine
         self.__engine = create_engine(
-            f'{dialect}+{driver}://{user}:\
-                {password}@{host}/{db}', pool_pre_ping=True)
-
+            f'{dialect}+{driver}://{user}:{password}@{host}/{db}',
+            pool_pre_ping=True)
         # drop all the table when the environment varieble of HBNB_ENV is test
         if (env == 'test'):
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """Retreive all the data"""
-
         if cls is None:
             objs = self.__session.query().all()
         else:
@@ -42,7 +39,6 @@ class DBStorage:
                 cls = eval(cls)
             objs = self.__session.query(cls)
         return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
-
 
     def new(self, obj):
         """Add the the obj to the db"""
@@ -60,9 +56,7 @@ class DBStorage:
     def reload(self):
         """Create the session and tables"""
         Base.metadata.create_all(self.__engine)
-
         # create a session
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session_factory)()
-
