9e898ecf1605a2932e840733bf394cf046c47d43
mefdev <mohamedmeftah2018@gmail.com>
2023-11-23 05:21:29 +0100

    Create amenity table

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import env_storage
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity table"""
+    __tablename__ = 'amenities'
+    if env_storage == "db":
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
