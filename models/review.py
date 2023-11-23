4865a10c6b7b6699445c2173651c2922bf904105
mefdev <mohamedmeftah2018@gmail.com>
2023-11-23 04:26:13 +0100

    Inherit from the Base

--- models/review.py
+++ models/review.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models import env_storage
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 
-class Review(BaseModel):
+
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     if env_storage == "db":
