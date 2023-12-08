c1b4b47dc3416667ac1ed2dd7ec191bd0ffa7828
mefdev <mohamedmeftah2018@gmail.com>
2023-12-08 20:41:27 +0100

    Pack a web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,6 +2,7 @@
 """Pack web_static folder to an archive"""
 from fabric.api import *
 import time
+import os
 
 
 def do_pack():
@@ -11,3 +12,11 @@ def do_pack():
     local("if [ ! -d ./versions/ ]; then mkdir versions; fi;")
     local('tar -cvzf versions/web_static_{}.tgz ./web_static'
           .format(current_time))
+    file_path = './1-pack_web_static.py'
+    if os.path.exists(file_path):
+        file_size = os.path.getsize(file_path)
+        print('web_static packed: versions/web_static_{}.tgz -> {}Bytes'
+              .format(current_time, file_size))
+        return f'versions/web_static_{current_time}.tgz'
+    else:
+        return 'None'
