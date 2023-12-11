2535ea63b8d4345574b71abeb4d0be4a0c8d1b71
mefdev <mohamedmeftah2018@gmail.com>
2023-12-11 22:02:00 +0100

    setup a web_static folder

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-#setup a web static in the two servers
+# setup a web static in the two servers
+
 USER='ubuntu'
 sudo apt-get -y update
 sudo apt-get -y install nginx
@@ -8,5 +9,27 @@ sudo mkdir -p /data/web_static/releases/test/
 echo "Hello Holberton" > /data/web_static/releases/test/index.html      
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R "$USER:$USER" '/data/'
-sed -i "12alocation /hbnb_static {\n\talias /data/web_static/current;\n\tautoindex off;\n}" "/etc/nginx/sites-available/default"
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm, index.nginx-debian.html;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm index.nginx-debian.html;
+    }
+
+    location /redirect_me {
+        return 301 http://hbnbairbnb.com;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 sudo service nginx restart
