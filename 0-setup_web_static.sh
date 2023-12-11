90c039ffc5301256d43656a8bb996c96d97dcd6a
mefdev <mohamedmeftah2018@gmail.com>
2023-12-11 03:12:19 +0100

    Setup web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
 #setup a web static in the two servers
-
-
 USER='ubuntu'
-
-# update and install nginx
 sudo apt-get -y update
 sudo apt-get -y install nginx
-
 sudo service nginx start
-sudo mkdir -p '/data/'
 sudo chown -R "$USER:$USER" '/data/'
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 echo "Hello Holberton" > /data/web_static/releases/test/index.html      
-
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Modify Nginx configuration using sed
 sed -i "12alocation /hbnb_static {\n\talias /data/web_static/current;\n\tautoindex off;\n}" "/etc/nginx/sites-available/default"
-
-# Restart the service
 sudo service nginx restart
