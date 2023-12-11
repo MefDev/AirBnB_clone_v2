c0bf33fd9f8add80a0e21da4cd94d0327bcedec0
mefdev <mohamedmeftah2018@gmail.com>
2023-12-11 01:34:32 +0100

    Deploy and pack a web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,10 +3,28 @@
 
 from fabric.api import *
 import os
+import time
 
 env.hosts = '54.237.23.212', '54.236.47.5'
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_pack():
+    """Pack a web_static folder to an archive"""
+    current_time = time.strftime('%Y%m%d%H%M%S')
+    print('Packing web_static to versions/web_static_{}'.format(current_time))
+    local("if [ ! -d ./versions/ ]; then mkdir versions; fi;")
+    local('tar -cvzf versions/web_static_{}.tgz ./web_static'
+          .format(current_time))
+    file_path = './1-pack_web_static.py'
+    if os.path.exists(file_path):
+        file_size = os.path.getsize(file_path)
+        print('web_static packed: versions/web_static_{}.tgz -> {}Bytes'
+              .format(current_time, file_size))
+        return f'versions/web_static_{current_time}.tgz'
+    else:
+        return 'None'
 
 
 def do_deploy(archive_path):
