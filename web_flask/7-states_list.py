023f6c185e2104d8c887415094603334b8f70bae
mefdev <mohamedmeftah2018@gmail.com>
2023-12-25 23:51:18 +0100

    Display states from DB

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,3 +14,13 @@ def display_states():
     """Show the states from the DB"""
     states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close the storage"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000)
