e1a562a1a7a88e11368ddf9c6fd4a86a27db3ffe
mefdev <mohamedmeftah2018@gmail.com>
2023-12-25 23:46:00 +0100

    Display states from Db

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,14 +3,14 @@
     that listens on a localhost port 5000
     """
 from flask import Flask, render_template
-from models import storage, state
-
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def display_states():
     """Show the states from the DB"""
-    states = storage.all(state.State).values()
+    states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
