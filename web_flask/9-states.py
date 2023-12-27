1fe968cbe9576f7e9bff930ff41d9e8f4aca769e
mefdev <mohamedmeftah2018@gmail.com>
2023-12-27 20:08:41 +0100

    Serve states solely and cities & states seperatly

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,17 +4,25 @@
     """
 from flask import Flask, render_template
 from models import storage
+from markupsafe import escape
 from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
-@app.route("/states/<id>", strict_slashes=False)
-def display_states(id=None):
+def display_states():
     """Show the states from the DB"""
     states = storage.all(State).values()
-    return render_template("9-states.html", states=states, id=id)
+    return render_template("9-states.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def display_state(id):
+    """Show a state from the DB"""
+    state = storage.all(State).get("State.{}".format(escape(id)))
+    print(state)
+    return render_template("9-states.html", state=state)
 
 
 @app.teardown_appcontext
